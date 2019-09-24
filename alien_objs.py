8d4fc017fd4895db9a5f3a5a71a94f1bf9e99f20
msamatar0 <mohamedsamatar100@gmail.com>
2019-09-23 17:52:22 -0700

    Added Objs

--- alien_objs.py
+++ alien_objs.py
@@ -6,6 +6,7 @@ class Settings():
     self.screen_width = 900
     self.screen_height = 600
     self.bg_color = (230, 230, 230)
+    self.speed = .8
 
 class Ship():
   def __init__(self, screen):
@@ -21,7 +22,7 @@ class Ship():
   def update(self):
     if self.right:
       self.rect.centerx += 1
-    elif self.left:
+    if self.left:
       self.rect.centerx -= 1
 
     if self.rect.x < 0:
@@ -36,15 +37,17 @@ def check_events(ship):
   for event in pygame.event.get():
       if event.type == pygame.QUIT:
         sys.exit()
+
       elif event.type == pygame.KEYDOWN:
         if event.key == pygame.K_RIGHT:
           ship.right = True
-        if event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_LEFT:
           ship.left = True
+
       elif event.type == pygame.KEYUP:
         if event.key == pygame.K_RIGHT:
           ship.right = False
-        if event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_LEFT:
           ship.left = False
 
 def update_screen(config, screen, ship):
